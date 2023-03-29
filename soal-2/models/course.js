ce1c7c241b2ad4315042b9a716f42d11e3d79fd9
eduardusoldi <eduardusoldi98@gmail.com>
2023-03-29 13:07:52 +0700

    feat: setting up model and database

---
+++ soal-2/models/course.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Course.hasMany(models.StudentCourse)
+    }
+  }
+  Course.init({
+    name: DataTypes.STRING,
+    code: DataTypes.STRING,
+    credit: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Course',
+  });
+  return Course;
+};
---
+++ soal-2/models/student.js
@@ -0,0 +1,93 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const { hash } = require('../helpers/bcryptjs');
+module.exports = (sequelize, DataTypes) => {
+  class Student extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Student.hasMany(models.StudentCourse)
+    }
+  }
+  Student.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Please insert student's name`
+        },
+        notEmpty: {
+          msg: `Please insert student's name`
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: `Student's email must be unique`
+      },
+      validate: {
+        notNull: {
+          msg: `Please insert student's e-mail`
+        },
+        notEmpty: {
+          msg: `Please insert student's e-mail`
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Please insert student's password`
+        },
+        notEmpty: {
+          msg: `Please insert student's password`
+        }
+      }
+    },
+    NIM: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Please insert student's NIM`
+        },
+        notEmpty: {
+          msg: `Please insert student's NIM`
+        }
+      }
+    },
+    major: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Please insert student's major`
+        },
+        notEmpty: {
+          msg: `Please insert student's major`
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Student',
+  });
+
+  Student.beforeCreate(student => {
+    student.password = hash(student.password)
+    return student.password
+  })
+  
+  return Student;
+};
